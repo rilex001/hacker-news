baf0e6eea5fb81cf06b9ded12ad78c7145964945
Nikola <ristic1900@gmail.com>
2020-05-27 22:49:47 +0200

    don't work scroll

--- src/containers/StoriesContainers.js
+++ src/containers/StoriesContainers.js
@@ -1,21 +1,29 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useMemo } from 'react'
 import { getStoryIds } from '../services/hnApi'
 import { Story } from '../components/Story'
+import { 
+        GlobalStyle, 
+        StoriesContainerWrapper
+} from '../styles/StoriesContainerStyles';
+import { useInfiniteScroll } from '../hooks/usingInfiniteScrooll'
 
 export const StoriesContainer = () =>  {
+    const { count } = useInfiniteScroll();
     const [storyIds, setStoryIds] = useState([]);
 
     useEffect(() => {
         getStoryIds().then(({ data }) => setStoryIds(data));
-        // getStory(20970623).then(data => console.log(data))
     }, [])
 
     return (
         <>
+        <GlobalStyle />
+        <StoriesContainerWrapper data-test-id='stories-container'>
         <h1>Hacker News Stories</h1>
-        {storyIds.map(storyId => (
+        {storyIds.slice(0, count).map(storyId => (
             <Story key={storyId} storyId={storyId}/>
         ))}
+        </StoriesContainerWrapper>
         </>
     )
 }
