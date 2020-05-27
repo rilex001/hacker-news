f6665396921071ad099d8f120a037bda762cc08e
Nikola <ristic1900@gmail.com>
2020-05-27 18:44:29 +0200

    stories data

--- src/containers/StoriesContainers.js
+++ src/containers/StoriesContainers.js
@@ -1,16 +1,21 @@
 import React, { useEffect, useState } from 'react'
-import { getStoryIds, getStory } from '../services/hnApi'
+import { getStoryIds } from '../services/hnApi'
+import { Story } from '../components/Story'
 
 export const StoriesContainer = () =>  {
     const [storyIds, setStoryIds] = useState([]);
 
     useEffect(() => {
         getStoryIds().then(({ data }) => setStoryIds(data));
-        getStory(20970623).then(data => console.log(data))
+        // getStory(20970623).then(data => console.log(data))
     }, [])
 
     return (
-
-     <p>{JSON.stringify(storyIds)}</p>
-     )
+        <>
+        <h1>Hacker News Stories</h1>
+        {storyIds.map(storyId => (
+            <Story key={storyId} storyId={storyId}/>
+        ))}
+        </>
+    )
 }
